555758eecf66586df86f0df0add2f5972a36b422
DerWeiseTeufel <76570054+DerWeiseTeufel@users.noreply.github.com>
2022-06-03 18:31:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using bst;
+var bst = new BST<int>();
+string path = "C:/Users/born2befree/source/repos/bst/bst/nodes.txt";
+
+using (var fin = new StreamReader(path))
+{
+    var text = fin.ReadToEnd();
+    var nodes = text.Split(new char[] { ' ','\n' }, StringSplitOptions.RemoveEmptyEntries);
+    foreach (var node in nodes)
+    {
+        bst.Add(int.Parse(node));
+    }
+
+}
+bst.Add(1000);
+bst.InOrder();
+var res = bst.caniadd();
+Console.WriteLine(res);
